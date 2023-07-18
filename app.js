d30bcb55ef4c77c2eb9294a2a98572aed67c3183
ankit rajput <ankitrajput72470788@gmail.com>
2023-07-18 14:27:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const heading1 = React.createElement("h1", {id:"title1"}, "namaste react");
+const heading2 = React.createElement("h2", {id:"title2"}, "namaste react");
+
+const container = React.createElement("div", {id:"container"}, [heading1, heading2]);
+console.log(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+
+root.render(container);
