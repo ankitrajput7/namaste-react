33523d575be1d2755e6f9c0a5befa952f2b6fc4f
ankit rajput <ankitrajput72470788@gmail.com>
2023-07-19 15:24:42 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const heading1 = React.createElement("h1", {id:"title1"}, "namaste react");
-const heading2 = React.createElement("h2", {id:"title2"}, "namaste react");
+import React from "react";
+import ReactDOM from "react-dom";
 
-const container = React.createElement("div", {id:"container"}, [heading1, heading2]);
+const heading1 = React.createElement("h1", { id: "title1" }, "namaste react");
+const heading2 = React.createElement("h2", { id: "title2" }, "namaste react");
+
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
 console.log(container);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
