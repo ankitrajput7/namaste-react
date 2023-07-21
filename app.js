0598ed97ab4cf924d72f4e02920add78773cc01e
ankit rajput <ankitrajput72470788@gmail.com>
2023-07-21 20:36:22 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title1" }, "namaste react");
-const heading2 = React.createElement("h2", { id: "title2" }, "namaste react");
 
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
-console.log(container);
+
+const heading1 = <h1>Hello</h1>;
+/**
+ * here heading1 is is a react element.
+ * heading1 at the end is a javascript object.
+ */
+
+
+
+const Heading = () => {
+  return (
+    <div>
+      {heading1}
+      <h1>functional component</h1>
+    </div>
+  );
+
+  /**
+   * the return code is a piece of JSX.
+   */
+};
+/**
+ * here heading is a example of functional component.
+ */
+console.log(Heading);
+
+
+
+const IndexFunction = () => {
+  return (
+    <>
+      <Heading></Heading>
+    </>
+  );
+};
+/*
+here IndexFunction is a example of function composition.
+*/
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-
-root.render(container);
+root.render(<IndexFunction />);
